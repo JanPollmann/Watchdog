e7dd62f1ce32e74183516b89302f062836168f48
Jan <pollmannjanf@gmail.com>
2023-01-13 14:23:47 +0100

    improved documentation

--- Library/src/main/java/de/pollmann/watchdog/util/statistics/NoStatistics.java
+++ Library/src/main/java/de/pollmann/watchdog/util/statistics/NoStatistics.java
@@ -1,5 +1,12 @@
 package de.pollmann.watchdog.util.statistics;
 
+import de.pollmann.watchdog.WatchableOptions;
+import de.pollmann.watchdog.WatchdogFactory;
+import de.pollmann.watchdog.tasks.Watchable;
+
+/**
+ *  Do nothing implementation for {@link WatchdogFactory#waitForCompletion(WatchableOptions, Watchable)} or {@link WatchdogFactory#submitFunctionCall(WatchableOptions, Watchable)}
+ */
 public class NoStatistics implements Statistics {
   @Override
   public Memento beginCall() {
